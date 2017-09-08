9163be2703aba464bd2e6458c1b512dbaaf8ccd3
LunaFr0st <codyamies@gmail.com>
2017-09-08 15:56:48 +1000

    Added Fonts Added GUIManager Used GUIStyles for Buttons/Text

---
+++ Assets/README.txt
@@ -0,0 +1,8 @@
+Layout
+	I am going to have my layout as a minimalistic style menu, this will give it a clean and modern look to it which may intrigue players to play the game.
+	The Menu will have the minimalistic look similar to Deceit (Survival Horror Game) but will have the logo off centered to the right with the Buttons on the left.
+	The Options menu will follow the similar look to the Main menu and have minimal amount of buttons to make it easier for the end user.
+	The Main menu will have a Side Expander which will contain News and Upcoming DLC Pages.
+	
+	The pause menu will have a carousel style to it following the Minimalist theme of the main menu, each page of the pause menu (Options, Save/load etc.) will circle around to allow you to access it
+	when the pause menu is active the screen will get a black tint to it to allow the user to know they are in the pause menu if they arent looking directly at it. 
---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AssignmentGUI
+{
+    public class GUIManager : MonoBehaviour
+    {
+        public GUIStyle titleStyler = new GUIStyle();
+        public GUIStyle styler = new GUIStyle();
+        public Font font;
+
+        private float sW;
+        private float sH;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            sW = Screen.width / 16;
+            sH = Screen.height / 9;
+        }
+
+        void OnGUI()
+        {
+            titleStyler.fontSize = (Screen.width / 16) + (Screen.height / 9);
+            titleStyler.normal.textColor = Color.white;
+            titleStyler.font = font;
+
+            styler.fontSize = ((Screen.width / 24) + (Screen.height / 11));
+            styler.normal.textColor = Color.grey;
+            styler.font = font;
+            styler.hover.textColor = Color.white;
+            styler.active.textColor = Color.gray;
+
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+            GUI.Label(new Rect(0.25f * sW, 3f * sH, 0, 0), "Roger That!", titleStyler);
+            if (GUI.Button(new Rect(0.25f * sW, 5f * sH, 3 * sW, 0.5f * sH), "Play", styler))
+            {
+
+            }
+        }
+    }
+}
+
